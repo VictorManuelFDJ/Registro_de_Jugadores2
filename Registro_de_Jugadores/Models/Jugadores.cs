6738ec548bdf17b554af2f5a8e2229b344a43338
Victor Manuel Frias De Jesús <victor_frias1@ucne.edu.do>
2025-09-12 17:18:22 -0400

    Mejoras#3, esta mojara trae iconos y una mejor optimizacion de codigo y mejoras de varios metonos

--- Registro_de_Jugadores/Models/Jugadores.cs
+++ Registro_de_Jugadores/Models/Jugadores.cs
@@ -8,6 +8,6 @@ namespace Registro_de_Jugadores.Models;
         [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
         public string Nombre { get; set; } = string.Empty;
         [Required(ErrorMessage = "El campo Partidas es obligatorio.")]
-        [Range(0, int.MaxValue, ErrorMessage = "La cantidad de partidas debe ser un número positivo.")]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad de partidas debe ser un número positivo.")]
         public int Partidas { get; set; }
     }
--- Registro_de_Jugadores/Services/JugadoresServices.cs
+++ Registro_de_Jugadores/Services/JugadoresServices.cs
@@ -5,84 +5,64 @@ using System.Linq.Expressions;
 
 namespace Registro_de_Jugadores.Services;
 
-    public class JugadoresServices(IDbContextFactory<Contexto> DbFactory)
+public class JugadoresServices(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<bool> Guardar(Jugadores jugador)
     {
-        
-        private async Task<bool> Existe(int jugadorId)
+        if (!await Existe(jugador.Nombre))
         {
-           await using var context = await DbFactory.CreateDbContextAsync();
-            return await context.Jugadores
-                .AnyAsync(j => j.JugadorId == jugadorId);
+            return await Insertar(jugador);
         }
-
-        
-        private async Task<bool> NombreExiste(string nombre, int jugadorId = 0)
+        else
         {
-             await using var context = await DbFactory.CreateDbContextAsync();
-            
-            return await context.Jugadores
-                .AnyAsync(j => j.Nombre.ToLower() == nombre.ToLower() && j.JugadorId != jugadorId);
+            return await Modificar(jugador);
         }
-
-        public async Task<bool> Guardar(Jugadores jugador)
-        {
-            if (await NombreExiste(jugador.Nombre!, jugador.JugadorId)) 
-            {
-               return false;
-            }
-            if (jugador.JugadorId == 0)
-            {
-                return await Insertar(jugador);
-            }
-            else 
-            {
-                if(!await Existe(jugador.JugadorId)) 
-                { 
-                  return false;
-                }
-                return await Modificar(jugador);
-            }
-        }
-
-        private async Task<bool> Insertar(Jugadores jugador)
-        {
-            await using var context = await DbFactory.CreateDbContextAsync();
-            context.Add(jugador);
-            return await context.SaveChangesAsync() >0;
-            
-        }
-
-        private async Task<bool> Modificar(Jugadores jugador)
-        {
-           await using var context = await DbFactory.CreateDbContextAsync();
-            context.Update(jugador);
-            return  await context.SaveChangesAsync() >0;
-            
-        }
-
-        public async Task<bool> Eliminar(int jugadorId)
-        {
-            await using var context = await DbFactory.CreateDbContextAsync();
-            return await context.Jugadores.Where
-                (j => j.JugadorId == jugadorId)
-                .ExecuteDeleteAsync() > 0;
-        }
-
-        public async Task<Jugadores?> Buscar(int jugadorId)
-        {
-            await using var context = await DbFactory.CreateDbContextAsync();
-        return await context.Jugadores.AsNoTracking().FirstOrDefaultAsync(j => j.JugadorId == jugadorId);
-                
-        }
-
-        public async Task<List<Jugadores>> Listar(Expression<Func<Jugadores, bool>> criterio)
-        {
-           await using var context =await  DbFactory.CreateDbContextAsync();
-            return await context.Jugadores
-                .Where(criterio)
-                .AsNoTracking()
-                .ToListAsync(); 
-        }
-
-       
     }
+
+    public async Task<bool> Insertar(Jugadores jugador)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Jugadores.Add(jugador);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Modificar(Jugadores jugador)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Update(jugador);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<Jugadores> Buscar(int jugadorId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Jugadores.FirstOrDefaultAsync(j => j.JugadorId == jugadorId);
+    }
+
+    public async Task<bool> Existe(string nombres)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Jugadores
+            .AnyAsync(j => j.Nombre == nombres);
+    }
+
+    public async Task<bool> Eliminar(int jugadorId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Jugadores.
+            AsNoTracking()
+            .Where(j => j.JugadorId == jugadorId)
+            .ExecuteDeleteAsync() > 0;
+    }
+
+    public async Task<List<Jugadores>> Listar(Expression<Func<Jugadores, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Jugadores
+            .Where(criterio)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+
+}
