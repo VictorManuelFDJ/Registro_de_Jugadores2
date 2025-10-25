f18ace71548640caaed1bb964968a451cfcaf9bb
Victor Manuel Frias De Jesús <victor_frias1@ucne.edu.do>
2025-10-25 12:37:47 -0400

    Creacion de el nuevo proyecto WebAssebly y se agrego la API

---
+++ TicTacToe.BlazorWasm/Program.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using TicTacToe.BlazorWasm;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://gestionhuacalesapi.azurewebsites.net/") });
+
+await builder.Build().RunAsync();
