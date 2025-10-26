8fdf6710154c46a80311cc3894c74fd46197f84a
Victor Manuel Frias De Jesús <victor_frias1@ucne.edu.do>
2025-10-26 19:18:59 -0400

    Revert "Se agrego PartidaModal y TicTacToeComponente"
    This reverts commit c068248a89e4463f827f0ebd26602a1f7a24d07d.

--- TicTacToe.BlazorWasm/Program.cs
+++ TicTacToe.BlazorWasm/Program.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TicTacToe.BlazorWasm;
-using TicTacToe.BlazorWasm.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://gestionhuacalesapi.azurewebsites.net/") });
-builder.Services.AddScoped<IPartidasApiService, PartidasApiService>();
 
 await builder.Build().RunAsync();
